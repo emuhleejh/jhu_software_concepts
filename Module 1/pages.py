e910b180b058a1e018851d9497afa3d4466646a9
Emily <ehammer5@jh.edu>
2026-01-25 14:10:08 -0600

    Made significant improvements / changes now that I have internet access and have installed Flask. Reorganized structure, added CSS styling.

--- Module 1/pages.py
+++ Module 1/pages.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Blueprint, render_template
+from flask import request
 
-bp = Blueprint("pages", __name, template_folder = "templates")
+bp = Blueprint("pages", __name__, template_folder = "templates")
 
 @bp.route("/")
 
--- Module 1/run.py
+++ Module 1/run.py
@@ -8,3 +8,6 @@ def create_app():
     app.register_blueprint(pages.bp)
 
     return app
+
+if __name__ == "__main__":
+    create_app().run(host="0.0.0.0", port=8080, debug=True)
