8320d7d2a57da7d6e17c62c6974a9f2830f6d499
Emily <ehammer5@jh.edu>
2026-02-08 11:20:36 -0600

    Data fully loaded into database.

---
+++ Module 3/flask_app.py
@@ -0,0 +1,25 @@
+# Necessary modules to operate programs
+import os
+import psycopg
+from flask import Flask, render_template
+from query_data import Query
+
+page = Flask(__name__)
+
+dbname = "applicants"
+user = "postgres"
+password = "python"
+
+# Return 'home' html template when endpoint matches '/'
+@page.route("/")
+
+def home():
+    q_data = Query(dbname, user, password)
+    q_data.run_query()
+    count_f26 = q_data.ct_applicants_f26
+    print(count_f26)
+    return render_template("home.html", data=count_f26)
+
+# Run application
+if __name__ == "__main__":
+    page.run(host="0.0.0.0", port=8080, debug=True)
--- Module 3/query_data.py
+++ Module 3/query_data.py
@@ -1,122 +1,157 @@
 import psycopg
 import json
 
-connection = psycopg.connect(dbname = "applicants", user = "postgres")
+class Query():
+    def __init__(self, dbname, user, password):
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.ct_applicants_f26 = ""
+        self.pct_intl = ""
+        self.avg_gpa = ""
+        self.avg_gre = ""
+        self.avg_gre_v = ""
+        self.avg_gre_aw = ""
+        self.avg_us_gpa_f26 = ""
+        self.pct_accepted_f25 = ""
+        self.avg_accept_gpa_f26 = ""
+        self.ct_jhu_ms_cs = ""
+        self.ct_select_phd_cs = ""
+        self.ct_select_phd_cs_llm = ""
+        self.prog_most_accept = ""
+        self.prog_accept_ct = ""
+        self.prog_most_reject = ""
+        self.prog_reject_ct = ""
+        self.uni_most_accept = ""
+        self.uni_accept_ct = ""
+        self.uni_most_accept = ""
+        self.uni_accept_ct = ""
 
-with connection.cursor() as c:
-    
-    # Count of entries that have applied for Fall 2026
-    c.execute("""SELECT COUNT p_id
-              FROM results
-              WHERE term = 'Fall 2026')
-              """)   
-    row = c.fetchone()
-    ct_applicants_f26 = row[0]
-    
-    # Percentage of entries from international students
-    c.execute("""SELECT us_or_international, COUNT(p_id)
-              FROM results
-              GROUP BY us_or_international
-              ORDER BY us_or_international asc
-              """)
-    rows = c.fetchall()
-    us, intl = rows[0][1], row[1][1]
-    pct_intl = round(100 * intl / (us + intl), 2)
-    
-    # Average GPA, GRE, GRE V, and GRE AW of applicants
-    c.execute("""SELECT avg(gpa), avg(gre), avg(gre_v), avg(gre_aw)
-              FROM results
-              """)
-    row = c.fetchone()
-    avg_gpa, avg_gre, avg_gre_v, avg_gre_aw = row[0], row[1], row[2], row[3]
+    def run_query(self):
+        connection = psycopg.connect(dbname = self.dbname, user = self.user, password = self.password)
 
-    # Average GPA of American students in Fall 2026
-    c.execute("""SELECT avg(gpa)
-              FROM results
-              WHERE term = 'Fall 2026' AND us_or_international = 'American'
-              """)
-    row = c.fetchone()
-    avg_us_gpa_f26 = row[0]
-    
-    # Percent of entries in Fall 2025 that are Acceptances
-    c.execute("""SELECT status, COUNT(p_id)
-              FROM results
-              WHERE term = 'Fall 2025'
-              GROUP BY status
-              ORDER BY status asc
-              """)
-    row = c.fetchall()
-    accepted, all_others = row[0][1], (row[1][1] + row[2][1] + row[3][1])
-    pct_accepted = round(100 * accepted / (accepted + all_others), 2)
-    
-    # Average GPA of accepted students in Fall 2026
-    c.execute("""SELECT avg(gpa)
-              FROM results
-              WHERE term = 'Fall 2026' AND status = 'accepted'
-              """)
-    row = c.fetchone()
-    avg_accept_gpa_f26 = row[0]
+        with connection.cursor() as c:
+            
+            # Count of entries that have applied for Fall 2026
+            c.execute("""SELECT COUNT(p_id)
+                    FROM results
+                    WHERE term = 'Fall 2026';
+                    """)   
+            row = c.fetchone()
+            self.ct_applicants_f26 = row[0]
+            
+            # Percentage of entries from international students
+            c.execute("""SELECT us_or_international, COUNT(p_id)
+                    FROM results
+                    GROUP BY us_or_international
+                    ORDER BY us_or_international desc;
+                    """)
+            rows = c.fetchall()
+        #     print(rows)
+            intl, us = rows[0][1], rows[1][1]
+            self.pct_intl = round(100 * intl / (us + intl), 2)
+            
+            # Average GPA, GRE, GRE V, and GRE AW of applicants
+            c.execute("""SELECT avg(gpa), avg(gre), avg(gre_v), avg(gre_aw)
+                    FROM results;
+                    """)
+            row = c.fetchone()
+            self.avg_gpa, self.avg_gre, self.avg_gre_v, self.avg_gre_aw = row[0], row[1], row[2], row[3]
 
-    # Count of applicants to JHU for a Masters in Computer Science
-    c.execute("""SELECT COUNT p_id
-              FROM results
-              WHERE degree = 'Masters' AND degree IN ('Computer Science') AND program IN ('Johns Hopkins')
-              """)
-    row = c.fetchone()
-    ct_jhu_ms_cs = row[0]
+            # Average GPA of American students in Fall 2026
+            c.execute("""SELECT avg(gpa)
+                    FROM results
+                    WHERE term = 'Fall 2026' AND us_or_international = 'American';
+                    """)
+            row = c.fetchone()
+            self.avg_us_gpa_f26 = row[0]
+            
+            # Percent of entries in Fall 2025 that are Acceptances
+            c.execute("""SELECT status, COUNT(p_id)
+                    FROM results
+                    WHERE term = 'Fall 2025'
+                    GROUP BY status
+                    ORDER BY status asc;
+                    """)
+            row = c.fetchall()
+            accepted, all_others = row[0][1], (row[1][1] + row[2][1] + row[3][1])
+            self.pct_accepted_f25 = round(100 * accepted / (accepted + all_others), 2)
+            
+            # Average GPA of accepted students in Fall 2026
+            c.execute("""SELECT avg(gpa)
+                    FROM results
+                    WHERE term = 'Fall 2026' AND status = 'accepted';
+                    """)
+            row = c.fetchone()
+            self.avg_accept_gpa_f26 = row[0]
 
-    # Count of applicants to selected universities for a PhD in Computer Science
-    c.execute("""SELECT COUNT p_id
-              FROM results
-              WHERE degree = 'PhD' AND program IN ('Computer Science') AND program IN ('Georgetown', 'MIT', 'Stanford', 'Carnegie Mellon')
-              """)
-    row = c.fetchone()
-    ct_select_phd_cs = row[0]
+            # Count of applicants to JHU for a Masters in Computer Science
+            c.execute("""SELECT COUNT(p_id)
+                    FROM results
+                    WHERE degree = 'Masters' AND degree IN ('Computer Science') AND program IN ('Johns Hopkins');
+                    """)
+            row = c.fetchone()
+            self.ct_jhu_ms_cs = row[0]
 
-    # Count of applicants to selected universities for a PhD in Computer Science using LLM answers
-    c.execute("""SELECT COUNT p_id
-              FROM results
-              WHERE degree = 'PhD' AND llm_generated_program IN ('Computer Science') AND llm_generated_university IN ('Georgetown', 'MIT', 'Stanford', 'Carnegie Mellon')
-              """)
-    row = c.fetchone()
-    ct_select_phd_cs_llm = row[0]
+            # Count of applicants to selected universities for a PhD in Computer Science
+            c.execute("""SELECT COUNT(p_id)
+                    FROM results
+                    WHERE degree = 'PhD' AND program IN ('Computer Science') AND program IN ('Georgetown', 'MIT', 'Stanford', 'Carnegie Mellon');
+                    """)
+            row = c.fetchone()
+            self.ct_select_phd_cs = row[0]
 
-    # Program with the most acceptances
-    c.execute("""SELECT llm_generated_program, COUNT(p_id) as total_count
-              FROM results
-              WHERE status = 'Accepted'
-              GROUP BY llm_generated_program
-              ORDER BY total_count desc
-              """)
-    row = c.fetchone()
-    prog_most_accept, prog_accept_ct = row[0], row[1]
+            # Count of applicants to selected universities for a PhD in Computer Science using LLM answers
+            c.execute("""SELECT COUNT(p_id)
+                    FROM results
+                    WHERE degree = 'PhD' AND llm_generated_program IN ('Computer Science') AND llm_generated_university IN ('Georgetown', 'MIT', 'Stanford', 'Carnegie Mellon');
+                    """)
+            row = c.fetchone()
+            self.ct_select_phd_cs_llm = row[0]
 
-    # Program with the most rejections
-    c.execute("""SELECT llm_generated_program, COUNT(p_id) as total_count
-              FROM results
-              WHERE status = 'Rejected'
-              GROUP BY llm_generated_program
-              ORDER BY total_count desc
-              """)
-    row = c.fetchone()
-    prog_most_reject, prog_reject_ct = row[0], row[1]
+            # Program with the most acceptances
+            c.execute("""SELECT llm_generated_program, COUNT(p_id) as total_count
+                    FROM results
+                    WHERE status = 'Accepted'
+                    GROUP BY llm_generated_program
+                    ORDER BY total_count desc;
+                    """)
+            row = c.fetchone()
+            self.prog_most_accept, self.prog_accept_ct = row[0], row[1]
 
-    # University with the most acceptances
-    c.execute("""SELECT llm_generated_university, COUNT(p_id) as total_count
-              FROM results
-              WHERE status = 'Accepted'
-              GROUP BY llm_generated_university
-              ORDER BY total_count desc
-              """)
-    row = c.fetchone()
-    uni_most_accept, uni_accept_ct = row[0], row[1]
+            # Program with the most rejections
+            c.execute("""SELECT llm_generated_program, COUNT(p_id) as total_count
+                    FROM results
+                    WHERE status = 'Rejected'
+                    GROUP BY llm_generated_program
+                    ORDER BY total_count desc;
+                    """)
+            row = c.fetchone()
+            self.prog_most_reject, self.prog_reject_ct = row[0], row[1]
 
-    # University with the most rejections
-    c.execute("""SELECT llm_generated_university, COUNT(p_id) as total_count
-              FROM results
-              WHERE status = 'Rejected'
-              GROUP BY llm_generated_university
-              ORDER BY total_count desc
-              """)
-    row = c.fetchone()
-    uni_most_reject, uni_reject_ct = row[0], row[1]
+            # University with the most acceptances
+            c.execute("""SELECT llm_generated_university, COUNT(p_id) as total_count
+                    FROM results
+                    WHERE status = 'Accepted'
+                    GROUP BY llm_generated_university
+                    ORDER BY total_count desc;
+                    """)
+            row = c.fetchone()
+            self.uni_most_accept, self.uni_accept_ct = row[0], row[1]
+
+            # University with the most rejections
+            c.execute("""SELECT llm_generated_university, COUNT(p_id) as total_count
+                    FROM results
+                    WHERE status = 'Rejected'
+                    GROUP BY llm_generated_university
+                    ORDER BY total_count desc;
+                    """)
+            row = c.fetchone()
+            self.uni_most_reject, self.uni_reject_ct = row[0], row[1]
+
+            connection.commit()
+
+        c.close()
+        connection.close()
+
+        return self
