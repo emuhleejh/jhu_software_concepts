1feb6a3288d459c8bfd8d42bbad5adce80fffb88
Emily <ehammer5@jh.edu>
2026-02-16 15:08:35 -0600

    All testing working. Moving on to round out documentation.

--- module_4/tests/test_integration_end_to_end.py
+++ module_4/tests/test_integration_end_to_end.py
@@ -1,10 +1,15 @@
 import pytest
+from _pytest.monkeypatch import MonkeyPatch
 from src.flask_app import page
+import src.flask_app as flask_app
+import src.data_processing.load_data as load_data
 
 @pytest.fixture()
 def app():
     app = page
     app.config.update({"TESTING" : True,})
+    flask_app.dbname = "test_database"
+    load_data.applicant_data_file = "tests\\sample_llm_extend_applicant_data_multiple.json"
     yield app
 
 @pytest.fixture()
@@ -14,3 +19,36 @@ def client(app):
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
+
+@pytest.mark.integration
+def test_end_to_end(client):
+
+    monkeypatch = MonkeyPatch()
+    def mock_run_parser():
+        load_data.clear_data()
+        load_data.load_data(flask_app.dbname, flask_app.user, flask_app.password)
+
+    monkeypatch.setattr(flask_app, 'run_parser', mock_run_parser)
+
+    client.post("/pull-data/")
+    flask_app.cache["pull-in-progress"]=False
+    flask_app.cache["update-in-progress"]=False
+
+    client.post("/update-analysis/")
+
+    response = client.get("/")
+
+    assert b"Applicant Count: 3" in response.data
+
+@pytest.mark.integration
+def test_end_to_end_uniqueness(client):
+    monkeypatch = MonkeyPatch()
+    def mock_run_parser():
+        load_data.load_data(flask_app.dbname, flask_app.user, flask_app.password)
+
+    monkeypatch.setattr(flask_app, 'run_parser', mock_run_parser)
+
+    response = client.post("/pull-data/")
+    response = client.post("/pull-data/")
+
+    assert b"Applicant Count: 3" in response.data
