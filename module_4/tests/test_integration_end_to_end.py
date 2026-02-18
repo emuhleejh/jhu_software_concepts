52346b43b2ae3f3db34105538bf1a2c72bd604a5
Emily <ehammer5@jh.edu>
2026-02-17 21:02:17 -0600

    starting database

--- module_4/tests/test_integration_end_to_end.py
+++ module_4/tests/test_integration_end_to_end.py
@@ -31,32 +31,41 @@ def test_end_to_end(client):
     # Clear data
     load_data.clear_data()
 
+    # Create and run mock function for running parser in test database with test data
     monkeypatch = MonkeyPatch()
     def mock_run_parser():
         load_data.load_data(flask_app.dbname, flask_app.user, flask_app.password)
-
     monkeypatch.setattr(flask_app, 'run_parser', mock_run_parser)
-
     client.post("/pull-data/")
+
+    # Set cache statuses to not busy
     flask_app.cache["pull-in-progress"]=False
     flask_app.cache["update-in-progress"]=False
+
+    # Update analysis from test database
     client.post("/update-analysis/")
     response = client.get("/")
 
+    # Only four entries in test database, applicant count must = 4
     assert b"Applicant Count: 4" in response.data
 
 @pytest.mark.integration
 def test_end_to_end_uniqueness(client):
     
+    # Create and run mock function for running parser in test database with test data
     monkeypatch = MonkeyPatch()
     def mock_run_parser():
         load_data.load_data(flask_app.dbname, flask_app.user, flask_app.password)
-
     monkeypatch.setattr(flask_app, 'run_parser', mock_run_parser)
-
     response = client.post("/pull-data/")
+
+    # Set cache statuses to not busy
     flask_app.cache["pull-in-progress"]=False
     flask_app.cache["update-in-progress"]=False
+
+    # Attempt to post duplicate data
     response = client.post("/pull-data/")
 
+    # Only four entries in test database
+    # If uniqueness requirements are imposed, applicant count must = 4
     assert b"Applicant Count: 4" in response.data
